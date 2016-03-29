b41828ab11d587eb70206eb84a8159e4c83d2379
Travis Haley <travis@haleycomputersolutions.com>
2016-03-29 16:31:17 -0600

    Added Unit Test Project

---
+++ Oasis.Tests/Commands/CommandTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oasis.Engine.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oasis.Engine.Interfaces;
+
+namespace Oasis.Engine.Commands.Tests
+{
+    [TestClass()]
+    public class CommandTests
+    {
+
+        private PlayerCharter _player;
+        private const int PLAYER_ID_UNKNOWN = 1;
+
+        private void SetupTests()
+        {
+            _player = new PlayerCharter(PLAYER_ID_UNKNOWN, 10, 10, "unknown", 1, 0, 0);
+            _player.CurrentLocation = World.LocationByID(World.LOCATION_ID_HOME);
+            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_HEALING_POTION), 2));
+
+        }
+
+        [TestMethod()]
+        public void ExecuteExitCommand()
+        {
+            SetupTests();
+            IGameCommand sut = new Exit();
+            string[] args = { "Exit" };
+            bool result = sut.ExecuteCommand(args, _player);
+            Assert.IsTrue(result);
+            Assert.IsTrue(_player.CurrentHitPoints == 0);
+        }
+    }
+}
